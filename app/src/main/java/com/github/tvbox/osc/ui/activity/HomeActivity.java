34db38ebf64fc3070236e125a53d3bc93adf7e00
xihi <xuhui0607@gmail.com>
2025-11-20 22:59:33 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -78,15 +78,6 @@ import java.util.Objects;
 import me.jessyan.autosize.utils.AutoSizeUtils;
 
 public class HomeActivity extends BaseActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState == null) {
-            // 仅在初次创建时检查更新
-            checkPermissions();
-        }
-    }
-
     private LinearLayout topLayout;
     private LinearLayout contentLayout;
     private TextView tvDate;
